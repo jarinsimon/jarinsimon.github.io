0f9500472b4d2711743b1b89bcd33ad59b53b43d
Jarin Simon <jarin.simon@gmail.com>
2023-12-15 01:18:44 -0800

    projects resizing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,38 +9,40 @@ import TripplarLogo from "../images/TripplarLogo.png"
 function Projects(){
     return(
         <>
-            <div name="projects" className="snap-start snap-y h-screen w-full bg-[#eff1ee]">
-                <p className="font-header text-5xl pt-24">Projects</p>
-                <div className="flex mt-6 mx-auto grid grid-cols-1 gap-x-6 gap-y-10 w-11/12 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 xl:w-5/6">
+            <div name="projects" className="snap-start snap-y w-full bg-[#eff1ee] pb-[4%]">
+                <p className="font-header text-5xl pt-[4%] xl:pt-[6%]">Projects</p>
+                <div className="mt-[2%] mx-auto auto-rows-max grid grid-cols-2 gap-x-12 gap-y-10 w-5/6 xl:grid-cols-3 xl:w-3/4">
                     <Tile 
                         title='Resume Website' 
                         dates='Oct 2023-Present' 
-                        description='Designed and programmed personal portfolio website'
+                        description='- Designed and programmed personal portfolio website'
                         skills='JavaScript, React, Tailwind CSS'
                         image={website}
                         >
                     </Tile>
                     <Tile 
                         title='NutriAR' 
-                        dates='September 2022 - June 2023' 
-                        description='Developed Android app to increase user awareness of eating habits. 
-                        through nutrient info visualization with 3 other SCU students for Senior capstone project.'
-                        skills='C#, Unity, Augmented Reality'
+                        dates='Sept 2022 - Jun 2023' 
+                        description={(<>
+                            - Developed Android app to increase eating habit awareness.<br/>
+                            - Worked with 3 other SCU students for Senior capstone project.
+                        </>)}
+                        skills='C#, Unity'
                         image={nutriAR}>
                     </Tile>
                     <Tile 
                         title='Flight Tracker' 
-                        dates='November 2021 - March 2022' 
-                        description='Built a web app using Amadeus Travel APIs to display available flights based 
+                        dates='Nov 2021 - Mar 2022' 
+                        description='- Built web app using Amadeus Travel APIs to display available flights based 
                         on user preferences.'
                         skills='Bootstrap, JavaScript, Node.js'
                         image={flight}>
                     </Tile>
                     <Tile 
                         title='Tripplar' 
-                        dates='December 2020 - June 2021' 
-                        description='Created iOS app to connect students and
-                        nearby small businesses in Santa Clara with team of SCU students.'
+                        dates='Dec 2020 - Jun 2021' 
+                        description='- Created iOS app to connect students and
+                        Santa Clara small businesses with team of SCU students.'
                         skills='Figma, Flutter'
                         image={TripplarLogo}>
                     </Tile>
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -3,18 +3,21 @@ import React from "react";
 function Tile(props){
     return(
         <>
-            <div className="group relative border-solid border-[medium] border-black rounded-[2em] hover:shadow-2xl">
-                <div className="flex mx-auto w-5/6 mt-6 overflow-hidden rounded-[2em] bg-gray-200 lg:aspect-none lg:h-80">
-                    <img className="h-full w-full object-cover border-solid border-[medium] rounded-[2em]" src={props.image}/>
-                </div>
-                <div className="mt-4 flex justify-between">
-                    <div className="text-xl font-normal w-4/5 mx-auto my-0">
-                        <div className="text-2xl font-bold">{props.title}</div>
-                        <div className="pb-4">{props.dates}</div>
-                        <div className="pb-8">{props.description}</div>
-                        <div className="font-semibold mb-4">{props.skills}</div>
+            <div className="group relative border-solid border-[medium] border-black rounded-xl hover:shadow-2xl h-96 xl:max-w-sm">
+                <div className="flex flex-col h-full">
+                    <div className="flex mx-auto h-1/3 w-5/6 mt-6 overflow-hidden rounded-xl bg-gray-200">
+                        <img className="h-full w-full object-cover border-solid border-[medium] rounded-xl" src={props.image}/>
+                    </div>
+                    <div className="pt-2">
+                        <div className="text-md lg:text-lg font-normal w-11/12 mx-auto my-0">
+                            <div className="text-lg lg:text-xl font-bold">{props.title}</div>
+                            <div className="pb-2">{props.dates}</div>
+                            <div className="pb-4">{props.description}</div>
+                            <div className="font-semibold mb-4">{props.skills}</div>
+                        </div>
                     </div>
                 </div>
+            
             </div>
         </>
     )
